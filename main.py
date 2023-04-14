a8991eb1bb6101570cd6cae62b96f5578b0c74e5
ictf50 <bassamm043@gmail.com>
2023-04-14 13:29:30 +0300

    add stop story and finish and complete

--- main.py
+++ main.py
@@ -2,82 +2,126 @@ import speech_recognition as sr
 from gtts import gTTS
 import pygame
 import os
-import requests
+import threading
 from playsound import playsound
+from pathlib import Path
+import uuid
 
-# Define the directory where the stories are stored
-directory = "stories"
+class Saluting:
+    def __init__(self, greeting):
+        self.greeting = greeting
 
-# Get a list of all the files in the directory
-files = os.listdir(directory)
+    def greet(self):
+        tmp_file = f"tmp_{uuid.uuid4().hex}.mp3"
+        tts = gTTS(self.greeting)
+        tts.save(tmp_file)
+        pygame.mixer.init()
+        pygame.mixer.music.load(tmp_file)
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy():
+            pass
+        pygame.mixer.quit()
+        os.remove(tmp_file)
 
-# Loop through each file and read in the contents
-stories = {}
-for file in files:
-    # Check that the file is a text file
-    if file.endswith(".txt"):
-        # Open the file and read in the contents
-        with open(os.path.join(directory, file), "r") as f:
-            story = f.read()
-            stories[file[:-4]] = story
+class StoryPlayer:
+    def __init__(self, stories_dir):
+        self.stories_dir = stories_dir
+        self.stories = {}
+        self.stop_flag = False
+        self.finished = False
+        self.mixer_initialized = False
+        self.pause_event = threading.Event()
 
-# initialize the speech recognizer
-r = sr.Recognizer()
+        for file in Path(self.stories_dir).glob("*.txt"):
+            with open(file, "r") as f:
+                story = f.read()
+                self.stories[file.stem] = story
 
-def Saluting(saluting):
-    tts = gTTS(saluting)
-    tts.save('st.mp3')
-    pygame.mixer.init()
-    pygame.mixer.music.load('st.mp3')
-    pygame.mixer.music.play()
-    while pygame.mixer.music.get_busy():
-        pass
-    pygame.mixer.quit()
+    def play_story(self, story):
+        tmp_file = f"tmp_{uuid.uuid4().hex}.mp3"
+        tts = gTTS(self.stories[story])
+        tts.save(tmp_file)
+        pygame.mixer.init()
+        self.mixer_initialized = True
+        pygame.mixer.music.load(tmp_file)
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy() and not self.stop_flag:
+            pygame.time.Clock().tick(10)
+            self.pause_event.wait()
+        pygame.mixer.music.stop()
+        pygame.mixer.quit()
+        self.mixer_initialized = False
+        os.remove(tmp_file)
 
-# define a function to play the story
-def play_story(story):
-    # convert the story to speech
-    tts = gTTS(stories[story])
-    tts.save('story.mp3')
-    # play the speech
-    pygame.mixer.init()
-    pygame.mixer.music.load('story.mp3')
-    pygame.mixer.music.play()
-    while pygame.mixer.music.get_busy():
-        pass
-    pygame.mixer.quit()
+    def pause(self):
+        if self.mixer_initialized:
+            pygame.mixer.music.pause()
+            self.pause_event.clear()
 
-# call the Saluting function to greet the user
-Saluting("Welcome!")
+    def resume(self):
+        if self.mixer_initialized:
+            pygame.mixer.music.unpause()
+            self.pause_event.set()
 
-# loop forever to listen for voice commands
-while True:
-    with sr.Microphone() as source:
-        print("Listening...")
-        # listen for a voice command
-        audio = r.listen(source)
-        print(audio)  # print the captured audio
+class VoiceControl:
+    def __init__(self, saluting, story_player):
+        self.r = sr.Recognizer()
+        self.saluting = saluting
+        self.story_player = story_player
+        self.story_player.stop_flag = False
 
-        try:
-            # recognize the voice command
-            command = r.recognize_google(audio)
-            print(f"You said: {command}")
+    def start(self):
+        threading.Thread(target=self.saluting.greet).start()
 
-            # check if the command contains the word "salute"
-            if "salute" in command.lower():
-                # play a sound or speech clip that represents a salute
-                playsound('salute.mp3')
-            else:
-                # try to match the command to a story
-                for story in stories:
-                    if story in command.lower():
-                        print(f"Playing {story}...")
-                        play_story(story)
-                        break
-                else:
+        while True:
+            with sr.Microphone() as source:
+                print("Listening...")
+                audio = self.r.listen(source, timeout=1, phrase_time_limit=3)
+                print(audio)
+
+                try:
+                    command = self.r.recognize_google(audio)
+                    print(f"You said: {command}")
+
+                    if "stop" in command.lower():
+                        if self.story_player.finished:
+                            self.saluting.greeting = "nothing to stop, please try again."
+                            self.saluting.greet()
+                        else:
+                            self.story_player.pause()
+                    elif "complete" in command.lower():
+                        if self.story_player.finished:
+                            self.saluting.greeting = "nothing to complete, please try again."
+                            self.saluting.greet()
+                        else:
+                            self.story_player.resume()
+                    elif "finish" in command.lower():
+                        self.story_player.stop_flag = True
+                        self.story_player.finished = True
+                        self.story_player.pause_event.set()
+                        self.story_player.pause()
+                    elif "salute" in command.lower():
+                        playsound('salute.mp3')
+                    else:
+                        self.story_player.stop_flag = False
+                        self.story_player.finished = False
+                        for story in self.story_player.stories:
+                            if story in command.lower():
+                                print(f"Playing {story}...")
+                                threading.Thread(target=self.story_player.play_story, args=(story,)).start()
+                                break
+                        else:
+                            print("Sorry, I didn't understand that.")
+
+                except sr.UnknownValueError:
                     print("Sorry, I didn't understand that.")
+                except sr.RequestError:
+                    print("Sorry, I couldn't connect to the service.")
+                except sr.WaitTimeoutError:
+                    print("Timeout reached, please try again.")
 
-        except sr.UnknownValueError:
-            print("Sorry, I didn't understand that.")
-        except sr.RequestError:
-            print("Sorry, I couldn't connect to the service.")
+if __name__ == '__main__':
+    saluting = Saluting("Welcome!")
+    story_player = StoryPlayer("stories")
+    voice_control = VoiceControl(saluting, story_player)
+    voice_control.start()
